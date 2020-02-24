51b0abed5bcd8b9157f6a8d2d1845ff4ca8ae8cb
toliner <toliner.rkt@gmail.com>
2020-02-24 18:37:33 +0900

    fix settings of kotlin plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,2 @@
 group = "dev.toliner"
 version = "0.0.1"
-
-plugins {
-    kotlin("plugin.serialization") version "1.3.61"
-}
--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,3 +1,4 @@
 object Versions {
     const val serializetionsVersion = "0.14.0"
+    const val kotlinVersion = "1.3.61"
 }
